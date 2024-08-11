d99ec45ebc6d0388c87ca75fc7d179563523efe6
jinxin <jinxin@wisdomgarden.com>
2024-08-11 09:08:50 +0800

    shopping init

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { Button } from "view-design";
+
+import "view-design/dist/styles/iview.css";
+
+Vue.component("Button", Button);
 
 Vue.config.productionTip = false;
 
